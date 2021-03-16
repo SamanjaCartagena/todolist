a1230ce61d35da330ea740f4890ccc6039fb76dd
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-03-16 18:43:34 -0400

    todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
-  return (
+const [todos, setTodos]=useState(['Take Dogs for a walk','Take the rubbish out', 'Quazi wants to livestream today']);
+const [input, setInput]=useState('');
+
+const addTodo= (event) =>{
+  //this will fire off when we click the button
+  event.preventDefault();
+console.log('Button pressed');
+setTodos([...todos, input]);
+setInput('');
+}
+return (
     <div className="App">
-      Hi
+      <form>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map(todo =>(
+          <li>{todo}</li>
+        ))}
+         
+
+      </ul>
     </div>
   );
 }
